d74a86c06e35ccce573b3b3a4c861f07a6e624b5
HERTSAGIG <reubenlema@gmail.com>
2016-04-01 13:57:44 +0000

    Placing Bombs now works sort of

--- minesweeper.c
+++ minesweeper.c
@@ -1,49 +1,18 @@
 # include <stdio.h>
 
-int main()
-{
-	printf("please enter a grid size to play on type g int int \n");
-	int width;
-	int height;
-    int nargs;
-	char identifier;
-    char input[10];
-    int grid[100][100];
-    
-    fgets(input, sizeof(input), stdin);
-    nargs = sscanf(input, "%c %d %d\n", &identifier, &width, &height);
-    if(identifier!='g'){
-        printf("not a valid letter command \n");
-        return 1;
-    }
-    if (nargs<3){
-        printf("invalid format....");
-        return 1;
-    }
-    if (width>100 && height>100){
-        printf("grid is too big");
-        return 1;
-    }
-   
-    
-    makeArray(grid[100][100],width, height);
-    //printBoard();
-	//scanf("%c %d %d", &identifier, &width, &height );
-	//printf("the identifier was %c, the width  and height is %d, %d \n", identifier, width, height);
-	//makeArray(width,height);
-    //placeBombs();
 
-	return 0;
-}
+int width;
+int height;
 
-int makeArray(grid[100][100], int widthIn, int heightIn){
+
+void makeArray(grid[100][100] ,int widthIn, int heightIn){
      int width=widthIn+2; //plus two here so that the borders don't reduce the size of the game board
      int height=heightIn+2;
     char corner='+';
      char top='-';
      char sides='|';
      char stars= '*';
-
+     
      for (int i = 0; i < height; ++i)
      {   
          for (int j = 0; j < width; ++j)
@@ -65,6 +34,7 @@ int makeArray(grid[100][100], int widthIn, int heightIn){
         }
 
     }   
+    /*
      for (int k = 0; k< height; ++k)
      {
          for (int l = 0; l < width; ++l)
@@ -74,10 +44,11 @@ int makeArray(grid[100][100], int widthIn, int heightIn){
          
     printf("\n");
     }
-     return 0;
+    */
  }
- //checks whether the provided co-ordinates are valid or not. Returns 0 if they are valid, 1 otherwise.
- /*
+
+ //checks whether the provided co-ordinates are within the grid. Returns 0 if they are valid, 1 otherwise.
+ 
 int isValid (int width, int height){
     if (width!=0 && width!=Gwidth){
         if (height!=0 && height!=Gheight){
@@ -87,41 +58,120 @@ int isValid (int width, int height){
     }
     else return 1;
 }
-void placeBombs(){
+
+
+//Initialises the bombgrid with bomb values, each co-ordinate is stored consecutively in the array, so sorting of the x,y pair is needed later.
+void placeBombs(int bombgrid[20], int width, int height){
     int counter=0;
-    int width;
-    int height;
+    int Bwidth;
+    int Bheight;
     int nargs;
     char identifier;
     char input[10];
+    //char bombgrid[4];
 
-    while (counter<10){}
+    while (counter<2){
+    printf("Please enter Bomb co-ordinate number %d \n", counter+1);
     fgets(input, sizeof(input), stdin);
-    nargs = sscanf(input, "%c %d %d\n", &identifier, &width, &height);
-    if (identifier!='b' || isValid(width, height)==1){
-        printf("invalid input, try again");
+    nargs = sscanf(input, "%c %d %d\n", &identifier, &Bwidth, &Bheight);
+    if (identifier=='q'){
+        //(identifier!='b' || isValid(width, height)==1){
+        printf("invalid input, try again \n");
+        break;
+
         }
         else{
-
+            printf("Bomb placed at %d, %d, \n", Bwidth, Bheight);
+            bombgrid[counter*2]=Bwidth;
+            bombgrid[counter*2+1]=Bheight;
+            counter++;
         }
     }
+}
 
-int printBoard(){
+printBoard(grid[100][100], int width, int height){
 printf("this is coming from printboard function \n");
 
-printf("%c \n",Globalboard[4][4]);
-
-   /* for (int k = 0; k< Gheight; ++k)
+   for (int k = 0; k< height+2; ++k)
      {
-         for (int l = 0; l < Gwidth; ++l)
+         for (int l = 0; l < width+2; ++l)
          {
-             printf("%c", Globalboard[k][l]);       
+             printf("%c", grid[k][l]);       
         }
          
     printf("\n");
     }
-    return 0;
+}
+    
+	int main(void)
+{
+    printf("please enter a grid size to play on type g int int \n");
+
+    int nargs;
+    char identifier;
+    char input[10];
+    char grid[100][100];
+    int bombgrid[20];
+    
+    fgets(input, sizeof(input), stdin);
+    nargs = sscanf(input, "%c %d %d\n", &identifier, &width, &height);
+    if(identifier!='g'){
+        printf("not a valid letter command \n");
+        return 1;
+    }
+    if (nargs<3){
+        printf("invalid format....");
+        return 1;
+    }
+    if (width>100 && height>100){
+        printf("grid is too big");
+        return 1;
+    }
+
+    makeArray(grid, width, height);
+    printBoard(grid, width, height);
+    placeBombs(bombgrid, width, height);
+    
+
+  for (int i = 0; i < 4; ++i)
+  {
+      printf("Bomb %d is %d \n", i*2+1, bombgrid[i*2]);
+      printf("Bomb %d is %d \n", i*2+2, bombgrid[i*2+1]);
+  }
+  int totalMoves=height*width;
+  for (int j=0; j<totalMoves+1; ++j){
+
+
+  }
+bombCheck(bombgrid, xcord, ycord)
+    /*
+    makeArray(grid, width, height);
+    printBoard(grid, width, height);
+    int *bombGridStart= placeBombs(grid, width, height);
+    printf("%d, \n" ,*bombGridStart);
+
+
+for (int i = 0; i < 2; ++i)
+    {   
+        
+        printf("Bomb %d, is %c ,%c\n", i+1, bombgrid[ i*2], bombgrid[i*2+1]);
+    }
+
+
+    for (int i = 0; i < 2; ++i)
+    {   
+        
+        printf("Bomb %d, is %c ,%c\n", i+1, *bombGridStart+ i*2, *bombGridStart+(i*2)+1);
+    }
     */
-	
+
+
+    //scanf("%c %d %d", &identifier, &width, &height );
+    //printf("the identifier was %c, the width  and height is %d, %d \n", identifier, width, height);
+    //makeArray(width,height);
+    //placeBombs();
+
+    return 0;
+}
 
 
